7058d79af3b977b87379a1e613a660be6e1b0eef
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-09-13 22:33:32 +0800

    billing completed

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -16,7 +16,10 @@ class Header extends Component {
 				);
 			default:
 				return [
-					<li key="1"><Payments /></li>,
+					<li key="1">
+						<Payments />
+					</li>,
+					<li key="3" style={{ margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>,
 					<li key="2">
 						<a href="/api/logout">Logout</a>
 					</li>
@@ -25,7 +28,6 @@ class Header extends Component {
 	}
 
 	render() {
-		console.log(this.props);
 		return (
 			<nav>
 				<div className="nav-wrapper">
