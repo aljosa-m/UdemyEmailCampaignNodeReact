3d4a58ce8d98e7a537b578314e71ab0449272eb3
Aljosa Musikic <mr.aljosa@Aljosas-MacBook-Air.local>
2017-09-15 11:43:55 +0800

    backend mailer finalized

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,23 @@
+const keys = require('../../config/keys');
+
 module.exports = survey => {
-  return '<div>' + {survey.body} + '</div>'
+  return `
+    <html>
+      <body>
+        <div style="text">
+          <h3>I'd like your input</h3>
+          <p>Please answer the questions</p>
+          <o>${survey.body}</p>
+          <div>
+            <a href="${keys.redirectDomain/api/surveys/thanks}">Yes</a>
+          </div>
+          <div>
+            <a href="${keys.redirectDomain/api/surveys/thanks}">No</a>
+          </div>
+
+        </div>
+
+      </body>
+    </html>
+  `
 };
